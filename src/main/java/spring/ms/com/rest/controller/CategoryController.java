f158d09f12057677bf2acb5f42326025dd9298d8
t-ilieva <ilieva0249@gmail.com>
2023-06-05 17:07:19 +0300

    commit

--- src/main/java/spring/ms/com/rest/controller/CategoryController.java
+++ src/main/java/spring/ms/com/rest/controller/CategoryController.java
@@ -1,23 +1,23 @@
 package spring.ms.com.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import spring.ms.com.rest.request.CategoryRequest;
+import spring.ms.com.rest.response.CategoryResponse;
 import spring.ms.com.services.CategoryService;
 
-import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("")
+    @GetMapping("/categories")
     public String getAll(Model model){
         model.addAttribute("title", "Categories");
         model.addAttribute("categories", categoryService.getAll());
@@ -25,5 +25,27 @@ public class CategoryController {
         return "categories";
     }
 
+    @GetMapping("/addCategory")
+    public String create(Model model){
+        CategoryRequest categoryRequest = new CategoryRequest();
+        model.addAttribute("title", "Add Category");
+        model.addAttribute("categoryRequest", categoryRequest);
 
+        return "add_category";
+    }
+
+    @PostMapping("/addCategory")
+    public String save(@ModelAttribute("categoryRequest") CategoryRequest categoryRequest,
+                       RedirectAttributes redirectAttributes){
+        int id = categoryService.createCategory(categoryRequest);
+        Optional<CategoryResponse> category = categoryService.getById(id);
+        if(category.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Error adding a category!");
+        }
+        else{
+            redirectAttributes.addFlashAttribute("successMessage", "Added successfully!");
+        }
+        return "redirect:/addCategory";
+    }
 }
+
